a5c678e4034f6924d8d30ca01ae7010468c4019b
K31644 <K31644@Z790-2867007020>
2024-12-06 15:17:23 +0000

    add linkedlist and stuff

---
+++ ChessGridMVVM/DataTypes/LinkedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGridMVVM.DataTypes
+{
+    public class LL
+    {
+        private int _count;
+
+        public int Count
+        {
+            get { return CountNodes; }
+        }
+
+        public LL()
+        {
+
+        }
+
+        public void AddFirst()
+        {
+
+        }
+
+        public void AddLast()
+        {
+
+        }
+
+        public int CountNodes()
+        {
+            int count = 0;
+            return count;
+        }
+
+
+
+    }
+}
